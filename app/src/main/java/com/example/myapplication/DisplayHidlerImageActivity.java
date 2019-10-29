c2f01f46467ed3d48e0091499b7f4dbb269cd23c
georgi.spasov <georgi.spasov@mobisystems.com>
2019-10-29 20:36:15 +0200

    Showing a hidler picture when the other button is pressed

--- app/src/main/java/com/example/myapplication/DisplayHidlerImageActivity.java
+++ app/src/main/java/com/example/myapplication/DisplayHidlerImageActivity.java
@@ -2,7 +2,10 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class DisplayHidlerImageActivity extends AppCompatActivity {
 
@@ -10,5 +13,11 @@ public class DisplayHidlerImageActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_hidler_image);
+        // Get the Intent that started this activity and extract the string
+        Intent intent = getIntent();
+
+        // Capture the layout's TextView and set the string as its text
+        ImageView imageView=findViewById(R.id.imageView);
+        imageView.setImageResource(R.drawable.hitler);
     }
 }
